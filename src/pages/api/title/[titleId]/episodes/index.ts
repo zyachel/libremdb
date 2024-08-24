ed2f3edd2323249eada378b130951ee2618e9cb2
zyachel <aricla@protonmail.com>
2024-08-24 17:19:33 +0530

    fix(episodes): fix error handling

--- src/pages/api/title/[titleId]/episodes/index.ts
+++ src/pages/api/title/[titleId]/episodes/index.ts
@@ -4,7 +4,7 @@ import episodes from 'src/utils/fetchers/titleEpisodes';
 import getOrSetApiCache from 'src/utils/getOrSetApiCache';
 import { titleEpisodesKey } from 'src/utils/constants/keys';
 import { keys } from 'src/utils/constants/titleEpisodesFilters';
-import { AppError, cleanQueryStr } from 'src/utils/helpers';
+import { AppError, cleanQueryStr, getErrorProperties } from 'src/utils/helpers';
 
 type ResponseData = { status: true; data: TitleEpisodes } | { status: false; message: string };
 
@@ -17,8 +17,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
     const queryStr = cleanQueryStr(queryObj, keys);
     const data = await getOrSetApiCache(titleEpisodesKey(titleId, queryStr), episodes, titleId, queryStr);
     res.status(200).json({ status: true, data });
-  } catch (error: any) {
-    const { message = 'Not found', statusCode = 404 } = error;
+  } catch (error) {
+    const { message, statusCode } = getErrorProperties(error);
     res.status(statusCode).json({ status: false, message });
   }
 }
--- src/utils/fetchers/titleEpisodes.ts
+++ src/utils/fetchers/titleEpisodes.ts
@@ -1,11 +1,10 @@
-import { AxiosError } from 'axios';
 import * as cheerio from 'cheerio';
-import axiosInstance from 'src/utils/axiosInstance';
+import axiosInstance, { isSaneError } from 'src/utils/axiosInstance';
 import { AppError } from 'src/utils/helpers';
 
 const episodes = async (titleId: string, queryStr = '') => {
   try {
-    // https://www.imdb.com/title/tt0364343/episodes?season=1
+    // https://www.imdb.com/title/tt2861424/episodes?season=1
     const res = await axiosInstance(`/title/${titleId}/episodes?${queryStr}`);
     const $ = cheerio.load(res.data);
 
@@ -18,11 +17,11 @@ const episodes = async (titleId: string, queryStr = '') => {
     };
 
     const episodes = getEpisodesList($main.find('article.episode-item-wrapper'), $);
-    const has_more_episodes = $main.find('.ipc-see-more').attr('class') !== undefined;
+    const hasMoreEpisodes = $main.find('.ipc-see-more').attr('class') !== undefined;
 
-    return { meta, episodes, has_more_episodes };
+    return { meta, episodes, has_more_episodes: hasMoreEpisodes };
   } catch (err) {
-    if (err instanceof AxiosError && err.response?.status === 404)
+    if (isSaneError(err) && err.response?.status === 404)
       throw new AppError('not found', 404, err.cause);
 
     if (err instanceof AppError) throw err;
@@ -39,7 +38,7 @@ const getEpisodesList = ($listItems: cheerio.Cheerio<cheerio.Element>, $: cheeri
   return $listItems
     .map((_i, el) => {
       const title = clean($(el).find('h4'));
-      const description = clean($(el).find('div[role="presentation"]'));;
+      const description = clean($(el).find('div[role="presentation"]'));
       const rating = clean($(el).find('div > .ipc-rating-star'));
       const date = clean($(el).find('span.sc-ccd6e31b-10'));
       const image = $(el).find('img').attr('src');
